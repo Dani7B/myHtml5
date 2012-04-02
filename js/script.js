251ac6594540851d584141f2332c4ce16cdf3f20
Daniele Morgantini <dan.morgantini@stud.uniroma3.it>
2012-04-02 10:17:33 +0200

    Aggiunto evento al bottone

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 !(function (exports) {
 
   console.log('it works!');
+  var button = $(#mybutton);
+
+  var saluta = function(event) {
+  	alert(Hello Web);
+  }
+
+  button.on("click",saluta);
 
 }(this));
