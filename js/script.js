a939c403762073b44834b94073e79ac2686a317c
Daniele Morgantini <dan.morgantini@stud.uniroma3.it>
2012-04-02 11:11:02 +0200

    Pulizia canvas

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
   var buttonWhite = $('#button-white'); // seleziona l'id
   var buttonGray = $('#button-gray');
+  var buttonClear = $('#clear');
   var containers = $('.container'); // seleziona la classe
   var canvas = $('#paper');
   var context = canvas[0].getContext("2d");
@@ -18,11 +19,18 @@
   	containers.addClass('bg-gray');
   });
 
+
+  buttonClear.on('click',function (event) { // evento e azione
+  	context.clearRect(0,0,canvas.width(), canvas.height()); // cheat
+  });
+
+
   var drawCircles = function(x, y, raggio) {
   	context.beginPath();
   	context.arc(x,y,raggio,0,2*Math.PI,true);
-  	context.stoke(); // fill se vogliamo il cerchio pieno
+  	context.stroke(); // fill se vogliamo il cerchio pieno
   };
+
 
   canvas.on('mousedown',function (event) {
   	drawCircles(event.offsetX, event.offsetY, 16);
