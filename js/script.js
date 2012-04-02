09762186df7aa68053c54ee917531e997988103e
Daniele Morgantini <dan.morgantini@stud.uniroma3.it>
2012-04-02 10:56:25 +0200

    Possibilit di disegnare cerchi

--- js/script.js
+++ js/script.js
@@ -3,17 +3,29 @@
   var buttonWhite = $('#button-white'); // seleziona l'id
   var buttonGray = $('#button-gray');
   var containers = $('.container'); // seleziona la classe
+  var canvas = $('#paper');
+  var context = canvas[0].getContext("2d");
 
 
-  buttonWhite.on('click',function(event) {
+  buttonWhite.on('click',function (event) {
   	containers.removeClass('bg-gray');
   	containers.addClass('bg-white');
   });
 
 
-  buttonGray.on('click',function(event) { // evento e azione
+  buttonGray.on('click',function (event) { // evento e azione
   	containers.removeClass('bg-white');
   	containers.addClass('bg-gray');
+  });
+
+  var drawCircles = function(x, y, raggio) {
+  	context.beginPath();
+  	context.arc(x,y,raggio,0,2*Math.PI,true);
+  	context.stoke(); // fill se vogliamo il cerchio pieno
+  };
+
+  canvas.on('mousedown',function (event) {
+  	drawCircles(event.offsetX, event.offsetY, 16);
   });
 
 }(this));
